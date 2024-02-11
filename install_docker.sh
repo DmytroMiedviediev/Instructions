25f0051f7c08d688b1b847e8f915b887de63deec
Dmytro Miedviediev <dmytro.miedviediev@gmail.com>
2024-02-11 21:36:16 +0000

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,1 +1,18 @@
+yes | sudo apt update;
 
+echo "apt-transport-https apt-transport-https/install-setuid boolean true" | sudo debconf-set-selections;
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install apt-transport-https;
+
+echo "ca-certificates ca-certificates/install-setuid boolean true" | sudo debconf-set-selections;
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install ca-certificates;
+
+echo "curl curl/install-setuid boolean true" | sudo debconf-set-selections;
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install curl;
+
+echo "software-properties-common software-properties-common/install-setuid boolean true" | sudo debconf-set-selections;
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install software-properties-common;
+
+yes | curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
+yes | sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable";
+apt-cache policy docker-ce;
+yes | sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin;
