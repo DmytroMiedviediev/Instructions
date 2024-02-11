4d3bfee792f34b667aac3b3eb155a6cafc4cb4bd
Dmytro Miedviediev <dmytro.miedviediev@gmail.com>
2024-02-11 12:38:38 +0000

    Update droplet_init.sh

--- droplet_init.sh
+++ droplet_init.sh
@@ -1,1 +1,3 @@
-
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+echo "root:temppasswordforauth" | chpasswd;
