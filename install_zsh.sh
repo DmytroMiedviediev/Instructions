415e6485805adaecec0514a8e3fd3d8d1a98446c
Dmytro Miedviediev <dmytro.miedviediev@gmail.com>
2024-02-19 09:43:48 +0000

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -4,3 +4,4 @@ chsh -s $(which zsh);
 yes | sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)";
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' /root/.zshrc;
 zsh;
+zsh;
