45f756ffdc6fb1ea6df1a507f2c981d93991c4fd
Dmytro Miedviediev <87547474+DmytroMiedviediev@users.noreply.github.com>
2024-07-18 15:22:07 +0200

    Update main_installs.sh

--- main_installs.sh
+++ main_installs.sh
@@ -1,3 +1,3 @@
-curl -s https://gitlab.com/dmytro.miedviediev/instructions/-/raw/main/reset_password.sh | bash -s;
-curl -s https://gitlab.com/dmytro.miedviediev/instructions/-/raw/main/install_zsh.sh | bash -s;
-curl -s https://gitlab.com/dmytro.miedviediev/instructions/-/raw/main/install_docker.sh | bash -s;
+curl -s https://raw.githubusercontent.com/DmytroMiedviediev/instructions/main/reset_password.sh | bash -s;
+curl -s https://raw.githubusercontent.com/DmytroMiedviediev/instructions/main/install_zsh.sh | bash -s;
+curl -s https://raw.githubusercontent.com/DmytroMiedviediev/instructions/main/install_docker.sh | bash -s;
