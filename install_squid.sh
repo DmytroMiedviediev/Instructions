4bd7192e5691642869133a2cb058066a4d4fd17e
Dmytro Miedviediev <87547474+DmytroMiedviediev@users.noreply.github.com>
2024-12-26 09:32:46 +0100

    Update install_squid.sh

--- install_squid.sh
+++ install_squid.sh
@@ -2,6 +2,27 @@ sudo apt update -y;
 sudo apt install squid -y;
 sudo systemctl stop squid;
 sudo rm /etc/squid/squid.conf;
-sudo bash -c 'echo "# Порти для HTTP і HTTPS\nhttp_port 3128\n\n# Дозволені порти (HTTP/HTTPS)\nacl Safe_ports port 80          # HTTP\nacl Safe_ports port 443         # HTTPS\nacl Safe_ports port 1025-65535  # Некоректні порти\nacl CONNECT method CONNECT\n\n# Доступ до HTTPS\nacl SSL_ports port 443\nhttp_access deny !Safe_ports\nhttp_access deny CONNECT !SSL_ports\n\n# Дозволити всі підключення\nhttp_access allow all\n\ncache_dir ufs /var/spool/squid 1000 16 256\ncache_mem 256 MB\nmaximum_object_size 50 MB" > /etc/squid/squid.conf';
+sudo tee /etc/squid/squid.conf > /dev/null <<EOF
+# Порти для HTTP і HTTPS
+http_port 3128
+
+# Дозволені порти (HTTP/HTTPS)
+acl Safe_ports port 80          # HTTP
+acl Safe_ports port 443         # HTTPS
+acl Safe_ports port 1025-65535  # Некоректні порти
+acl CONNECT method CONNECT
+
+# Доступ до HTTPS
+acl SSL_ports port 443
+http_access deny !Safe_ports
+http_access deny CONNECT !SSL_ports
+
+# Дозволити всі підключення
+http_access allow all
+
+cache_dir ufs /var/spool/squid 1000 16 256
+cache_mem 256 MB
+maximum_object_size 50 MB
+EOF
 sudo systemctl start squid;
 sudo systemctl enable squid;
